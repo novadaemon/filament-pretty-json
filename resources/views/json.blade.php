b295957f240cf848ffd402d842e9db6e7b5346f3
Jesús García <novadaemon@gmail.com>
2024-06-08 17:19:44 -0300

    fix: fix styles and div container classes

--- resources/views/json.blade.php
+++ resources/views/json.blade.php
@@ -5,7 +5,7 @@
             json = JSON.parse(this.state)
             return window.prettyPrint(json)
         }
-    }">
+    }" class="min-w-0 flex-1">
         <pre class="prettyjson" x-html="prettyJson"></pre>
     </div>
 </x-dynamic-component>
