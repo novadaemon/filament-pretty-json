e1577286c6b26d85ba0deaf95fcd13ee6d09c831
Jesús García <novadaemon@gmail.com>
2025-05-12 09:42:29 -0300

    fix: adding deprecated class to keep older implementations

---
+++ src/PrettyJson.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Novadaemon\FilamentPrettyJson;
+
+use Novadaemon\FilamentPrettyJson\Form\PrettyJsonField;
+
+/**
+ * @deprecated since 2.5.0 Use \App\FilamentPrettyJson\Form\PrettyJsonField instead.
+ */
+class PrettyJson extends PrettyJsonField
+{
+}
